f2ffeb419ee99ea7f43f657f6632b45625c3205d
GGonnerman <gaston95g@gmail.com>
2022-10-19 02:05:22 -0500

    Added base install and later install

---
+++ btrfs-luks-install.md
@@ -0,0 +1,415 @@
+# Partition disk
+
+### Figure out the drive that you are installing to and launch fdisk on it
+
+```
+fdisk /dev/sdX
+```
+
+### Enable GPT disklabel
+
+```
+g
+```
+
+### Create EFI Partition
+
+```
+n # Create new partion
+<enter> # Accept default partition number
+<enter> # Accept default first sector
++500M # Partition length of 500M
+t # Update partition type
+1 # Select 'EFI System'
+```
+
+### Create Boot Partition
+
+```
+n # Create new partion
+<enter> # Accept default partition number
+<enter> # Accept default first sector
++500M # Partition length of 500M
+```
+
+### Create primary (luks encrypted, btrfs) Partition
+
+```
+n # Create new partion
+<enter> # Accept default partition number
+<enter> # Accept default first sector
++SIZEG # Use most of the rest of the disk
+```
+
+### Create shared with windows (ntfs) partition
+
+```
+n # Create new partion
+<enter> # Accept default partition number
+<enter> # Accept default first sector
+<enter> # Use most of the rest of the disk
+t
+<enter>
+11 # Use Microsoft basic data format
+```
+
+### Write the partition scheme to the disk
+```
+w
+```
+
+### Full paritioning command summary
+
+```
+fdisk /dev/sdX
+g
+n
+<enter>
+<enter>
++500M
+t
+1
+n
+<enter>
+<enter>
++500M
+n
+<enter>
+<enter>
++SIZEG
+n
+<enter>
+<enter>
+<enter>
+t
+<enter>
+11
+w
+```
+
+# Format disk
+
+### Format EFI partition
+```
+mkfs.fat -F32 /dev/sdX1
+```
+
+### Format Boot partition
+```
+mkfs.ext4 /dev/sdX2
+```
+
+### Format NTFS partition
+```
+mkfs.ntfs /dev/sdX4
+```
+
+### Format BTRFS partition
+
+#### Setup LUKS
+```
+cryptsetup -y -v --cipher=aes-xts-plain64 --key-size=512 --hash=sha512 luksFormat /dev/sdX3
+```
+
+#### Mount LUKS
+```
+cryptsetup luksOpen /dev/sdX3 luks
+```
+
+#### Format BTRFS
+```
+mkfs.btrfs -L archlinux /dev/mapper/luks
+```
+
+#### Create BTRFS subvolumes
+```
+mount -o compress=zstd /dev/mapper/luks /mnt
+```
+
+#### Create subvolumes
+```
+cd /mnt
+btrfs subvolume create @
+btrfs subvolume create @home
+btrfs subvolume create @log
+btrfs subvolume create @srv
+btrfs subvolume create @pkg
+btrfs subvolume create @tmp
+btrfs subvolume create @images
+btrfs subvolume create @backup
+btrfs subvolume create @downloads
+cd /
+umount /mnt
+mount -o compress=zstd,subvol=@ /dev/mapper/luks /mnt
+```
+##### Copy-paste version:
+```
+cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home ; btrfs subvolume create @log ; btrfs subvolume create @srv ; btrfs subvolume create @pkg ; btrfs subvolume create @tmp ; btrfs subvolume create @images ; btrfs subvolume create @backup ; btrfs subvolume create @downloads ; cd / ; umount /mnt ; mount -o compress=zstd,subvol=@ /dev/mapper/luks /mnt
+```
+
+#### Create filesystem directories
+```
+cd /mnt
+mkdir -p {home,srv,var/{log,cache/pacman/pkg,lib/libvirt/images},tmp,backup}
+```
+
+#### Associate filesystem directories to subvolumes
+```
+mount -o compress=zstd,subvol=@home /dev/mapper/luks home
+mount -o compress=zstd,subvol=@log /dev/mapper/luks var/log
+mount -o compress=zstd,subvol=@pkg /dev/mapper/luks var/cache/pacman/pkg
+mount -o compress=zstd,subvol=@srv /dev/mapper/luks srv
+mount -o compress=zstd,subvol=@tmp /dev/mapper/luks tmp
+mount -o nodatacow,subvol=@images /dev/mapper/luks var/lib/libvirt/images
+mount -o nodatacow,subvol=@backup /dev/mapper/luks backup
+mkdir /mnt/boot
+mount /dev/vda2 /mnt/boot
+mkdir /mnt/boot/EFI
+mount /dev/vda1 /mnt/boot/EFI
+mkdir -p /mnt/mnt/ext
+mount -o uid=1000,gid=1000,dmask=022,fmask=027,windows_names /dev/vda4 /mnt/mnt/ext
+```
+##### Copy-paste version:
+```
+mount -o compress=zstd,subvol=@home /dev/mapper/luks home ; mount -o compress=zstd,subvol=@log /dev/mapper/luks var/log ; mount -o compress=zstd,subvol=@pkg /dev/mapper/luks var/cache/pacman/pkg ; mount -o compress=zstd,subvol=@srv /dev/mapper/luks srv ; mount -o compress=zstd,subvol=@tmp /dev/mapper/luks tmp ; mount -o nodatacow,subvol=@images /dev/mapper/luks var/lib/libvirt/images ; mount -o nodatacow,subvol=@backup /dev/mapper/luks backup ; mkdir /mnt/boot ; mount /dev/vda2 /mnt/boot ; mkdir /mnt/boot/EFI ; mount /dev/vda1 /mnt/boot/EFI ; mkdir -p /mnt/mnt/ext ; mount -o uid=1000,gid=1000,dmask=022,fmask=027,windows_names /dev/vda4 /mnt/mnt/ext
+```
+
+
+### Generate filesystem to table
+```
+mkdir /mnt/etc
+genfstab -U /mnt >> /mnt/etc/fstab
+```
+
+### Enable Parallel Downloads for pacstrap
+```
+sed -Ei "s/^#(ParallelDownloads).*/\1 = 8/;/^#Color$/s/#//" /etc/pacman.conf
+```
+
+# Up to this point summary:
+```
+fdisk /dev/sdX
+g
+n
+<enter>
+<enter>
++500M
+t
+1
+n
+<enter>
+<enter>
++500M
+n
+<enter>
+<enter>
++SIZEG
+n
+<enter>
+<enter>
+<enter>
+t
+<enter>
+11
+w
+```
+
+### Rest as copy-paste
+```
+mkfs.fat -F32 /dev/vda1 ; mkfs.ext4 /dev/vda2 ; mkfs.ntfs /dev/vda4 ; cryptsetup -y -v --cipher=aes-xts-plain64 --key-size=512 --hash=sha512 luksFormat /dev/vda3 ; cryptsetup luksOpen /dev/vda3 luks ; mkfs.btrfs -L archlinux /dev/mapper/luks ; mount -o compress=zstd /dev/mapper/luks /mnt ; cd /mnt ; btrfs subvolume create @ ; btrfs subvolume create @home ; btrfs subvolume create @log ; btrfs subvolume create @srv ; btrfs subvolume create @pkg ; btrfs subvolume create @tmp ; btrfs subvolume create @images ; btrfs subvolume create @backup ; btrfs subvolume create @downloads ; cd / ; umount /mnt ; mount -o compress=zstd,subvol=@ /dev/mapper/luks /mnt ; cd /mnt ; mkdir -p {home,srv,var/{log,cache/pacman/pkg,lib/libvirt/images},tmp,backup} ; mount -o compress=zstd,subvol=@home /dev/mapper/luks home ; mount -o compress=zstd,subvol=@log /dev/mapper/luks var/log ; mount -o compress=zstd,subvol=@pkg /dev/mapper/luks var/cache/pacman/pkg ; mount -o compress=zstd,subvol=@srv /dev/mapper/luks srv ; mount -o compress=zstd,subvol=@tmp /dev/mapper/luks tmp ; mount -o nodatacow,subvol=@images /dev/mapper/luks var/lib/libvirt/images ; mount -o nodatacow,subvol=@backup /dev/mapper/luks backup ; mkdir /mnt/boot ; mount /dev/vda2 /mnt/boot ; mkdir /mnt/boot/EFI ; mount /dev/vda1 /mnt/boot/EFI ; mkdir -p /mnt/mnt/ext ; mount -o uid=1000,gid=1000,dmask=022,fmask=027,windows_names /dev/vda4 /mnt/mnt/ext ; mkdir /mnt/etc ; genfstab -U /mnt >> /mnt/etc/fstab ; sed -Ei "s/^#(ParallelDownloads).*/\1 = 8/;/^#Color$/s/#//" /etc/pacman.conf
+```
+
+# Arch Install
+```
+pacstrap /mnt \
+  base base-devel \ # Base packages
+  linux linux-firmware linux-headers \ # Kernal
+  grub efibootmgr os-prober \ # Bootloader
+  ntfs-3g dosfstools mtools \ # File system tools for ms-dos
+  networkmanager wireless_tools wpa_supplicant \ # Enable (wireless) networking
+  neovim # Neovim text editor
+```
+#### Copy-paste version:
+```
+pacstrap /mnt base base-devel linux linux-firmware linux-headers grub efibootmgr os-prober ntfs-3g dosfstools mtools networkmanager wireless_tools wpa_supplicant neovim
+```
+
+## Change your "root" into the bootstrap system
+```
+arch-chroot /mnt
+```
+
+### Automatically startup wifi
+```
+systemctl enable NetworkManager
+```
+
+### Setup locale
+```
+nvim /etc/locale.gen
+```
+
+#### Uncomment the line
+> #en_US.UTF-8 UTF-8
+
+#### Generate locale
+```
+locale-gen
+```
+
+### Setup system locale
+```
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+```
+
+### Set root password
+```
+passwd
+```
+
+### Setup encryption in the initial RAM filesystem
+```
+nvim /etc/mkinitcpio.conf
+```
+
+#### Change the line
+> HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)
+
+#### to
+> HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)
+
+#### Generate new initramfs
+
+##### If installed linux
+```
+mkinitcpio -p linux
+```
+##### If installed linux-lts
+```
+mkinitcpio -p linux-lts
+```
+
+## Configure boot loader
+```
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+```
+
+### Configure the locale
+```
+mkdir -p /boot/grub/locale
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+```
+
+### Configure LUKS and grub
+
+#### Grab partition UUID
+```
+blkid /dev/sdX3
+```
+
+#### Should have output like
+> /dev/sdX3: UUID="ea3c5e4b-d8c7-4ee3-a1af-733cf80f8d44" TYPE="crypto_LUKS" PARTUUID="40d13c59-5146-0544-b694-38037ef9def0"
+
+#### Edit grub config file
+```
+nvim /etc/default/grub
+```
+
+##### Change line
+> GRUB_CMDLINE_LINUX=""
+
+##### to something like (with your uuid)
+> GRUB_CMDLINE_LINUX="cryptdevice=UUID=ea3c5e4b-d8c7-4ee3-a1af-733cf80f8d44:root"
+
+#### Enable cryptodisk
+
+##### Change line
+> #GRUB_ENABLE_CRYPTODISK=y
+
+#### to
+> GRUB_ENABLE_CRYPTODISK=y
+
+#### Generate the final config
+```
+grub-mkconfig -o /boot/grub/grub.cfg
+```
+
+# Test setup
+```
+exit
+cd /
+umount -R /mnt
+reboot
+```
+
+# Upon reboot
+
+### Active your connection
+```
+nmtui
+```
+
+### Run my startup script
+
+### Add a new user (and add to wheel group)
+```
+useradd -mG wheel twoonesecond
+```
+
+#### Setup password for the new user
+```
+passwd twoonesecond
+```
+
+#### Allow wheel group to use sudo
+```
+EDITOR=nvim visudo
+```
+
+##### Uncomment the line
+
+> \# %wheel ALL=(ALL:ALL) ALL
+
+### Dump of every post-install note
+
+### Create hostname file (will be shown to everyone on filesharing)
+```
+hostnamectl set-hostname myhostname
+```
+
+### Update the hosts file
+```
+nvim /etc/hosts
+```
+#### Set the contents to
+> 127.0.0.1	localhost\
+> 127.0.1.1	myhostname
+
+### Set the correct timezone
+```
+timedatectl set-timezone America/Chicago
+```
+
+### Allow timedate to work with windows dual boot
+```
+timedatectl set-local-rtc 1
+```
+
+### Sync time
+```
+hwclock --systohc
+```
+
+### Always start timesyncd
+```
+systemctl enable systemd-timesyncd
+```
+
+# TODO: Check if these packages are okay to be installed after
+>   pacman-contrib \ # 
+>   man-db \ # Man pages
+>   sudo \ # Allow super user privileges
+>   amd-ucode \ # Install amd microcode
+Then
+>   nvidia \ # if installed linux
+>   nvidia-lts \ # if installed linux-lts
---
+++ install.sh
@@ -0,0 +1,285 @@
+dotfilesrepo="https://github.com/GGonnerman/.dotfiles.git"
+progsfile="https://raw.githubusercontent.com/GGonnerman/ArchBootstrap/master/progs.csv"
+wallpaperrepo="https://github.com/GGonnerman/Wallpapers.git"
+
+error() {
+	# Log to stderr and exit with failure.
+	printf "%s\n" "$1" >&2
+	exit 1
+}
+
+givewarning() {
+    whiptail --title "WARNING" \
+        --yes-button "CONTINUE" \
+        --no-button "Return..." \
+        --yesno "Welcome to Gaston's arch configuration! WARNING: This script will overwrite various configurations and the entire $name user." 14 70
+}
+
+gethostname() {
+	myhostname=$(whiptail --nocancel --inputbox "Enter the hostname" 10 60 3>&1 1>&2 2>&3 3>&1)
+}
+
+getuserandpass() {
+	# Prompts user for new username an password.
+	name=$(whiptail --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
+	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
+		name=$(whiptail --nocancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+	pass1=$(whiptail --nocancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	while ! [ "$pass1" = "$pass2" ]; do
+		unset pass2
+		pass1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		pass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+}
+
+getveracryptpass() {
+	vpass1=$(whiptail --nocancel --passwordbox "Enter a password for veracrypt." 10 60 3>&1 1>&2 2>&3 3>&1)
+	vpass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	while ! [ "$vpass1" = "$vpass2" ]; do
+		unset vpass2
+		vpass1=$(whiptail --nocancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		vpass2=$(whiptail --nocancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+}
+
+givesecondwarning() {
+    whiptail --title "WARNING" \
+        --yes-button "Yes, Let's do this!" \
+        --no-button "No, Nevermind" \
+        --yesno "Are you sure this is what you want to do? There is no confirmation that this will not break your system." 14 70
+}
+
+querycpu() {
+	whiptail --title "CPU" \
+		--yes-button "amd" \
+		--no-button "intel" \
+		--yesno "Do you have an amd or intel cpu?" 14 70
+	if [[ $? -eq 0 ]]; then
+		cpu="amd"
+	elif [[ $? -eq 1 ]]; then
+		cpu="intel"
+	elif [[ $? -eq 255 ]]; then
+		error "User exited."
+	fi
+}
+
+adduserandpass() {
+    useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
+		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	export repodir="/home/$name/.local/src"
+	mkdir -p "$repodir"
+	chown -R "$name":wheel "$(dirname "$repodir")"
+	echo "$name:$pass1" | chpasswd
+	unset pass1 pass2
+}
+
+maininstall() {
+	whiptail --title "Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
+	paru --noconfirm --needed -S "$1" >/dev/null 2>&1
+}
+
+installationloop() {
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) ||
+		curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
+	total=$(wc -l </tmp/progs.csv)
+	aurinstalled=$(pacman -Qqm)
+	while IFS=, read -r tag program comment; do
+		n=$((n + 1))
+		echo "$comment" | grep -q "^\".*\"$" &&
+		comment="$(echo "$comment" | sed -E "s/(^\"|\"$)//g")"
+		maininstall "$program" "$comment"
+	done </tmp/progs.csv
+}
+
+givewarning || error "User exited."
+
+gethostname || error "User exited."
+
+getuserandpass || error "User exited."
+
+getveracryptpass || error "User exited."
+
+givesecondwarning || error "User exited."
+
+querycpu
+
+## Update pacman configuration settings
+
+### Enable multilib
+sed -i "/\[multilib\]/,/Include /"'s/^#//' /etc/pacman.conf
+
+### Enable chaotic aur
+if ! grep -q "chaotic-aur" /etc/pacman.conf; then
+	pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+	pacman-key --lsign-key FBA220DFC880C036
+	pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+	echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+fi
+
+### Make pacman colorful, concurrent downloads and Pacman eye-candy.
+grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+sed -Ei "s/^#(ParallelDownloads).*/\1 = 8/;/^#Color$/s/#//" /etc/pacman.conf
+
+### Use all cores for compilation.
+sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
+
+### Update pacman database
+sudo pacman -Syyy --noconfirm
+
+pacman --noconfirm -S archlinux-keyring || error "Error automatically refreshing Arch keyring"
+
+### Install a small number of progarms needed to install other programs
+for x in curl paru pacman-contrib zsh veracrypt btrfs-progs arch-install-scripts; do
+	pacman --noconfirm --needed -S "$x" >/dev/null 2>&1
+done
+
+### Sort the fastest mirrors
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 12 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+### Add the user account with specific password
+adduserandpass || error "Error adding username and/or password."
+
+### Umount the ntfs drive to its not open (and so its not in new etc/fstab)
+umount /dev/vda4
+
+### Use previously created (not nested) subvolume as download folder so its excluded from backups but isnt broken
+sudo -u "$name" mkdir -p /home/$name/downloads
+mount -o compress=zstd,subvol=@downloads /dev/mapper/root /home/$name/downloads
+chown -R "$name":wheel /home/$name/downloads
+genfstab -U / > /etc/fstab
+
+### DONT USE: This creates a NESTED SUBVOLUME STRUCTURE which seems to be annoying to deal with
+### Create downloads subvol so its excluded from home snapshots (because it contains large, non-critical files)
+# cd /home/$name
+# sudo -u "$name" btrfs subvolume create downloads
+# mount -o compress=zstd,subvol=@home/$name/downloads /dev/mapper/root /home/$name/downloads
+
+### Setup veracrypt encrypted directory
+#### Veracrypt encrypt it
+veracrypt -t -c --volume-type="Normal" /dev/vda4 --encryption="AES" --hash="SHA-512" --filesystem="ntfs" --password="$vpass1" --pim=0
+#### Unlock the now encrypted drive
+echo -n "$vpass1" | cryptsetup tcryptOpen /dev/vda4 ext
+#### Install ntfs on the unencrypted drive
+mkfs.ntfs /dev/mapper/ext
+#### Mount the unencrypted ntfs to the correct location
+mkdir /mnt/ext
+mount /dev/mapper/ext /mnt/ext
+#### Setup easy decryption for the future
+echo -e "\n/dev/mapper/ext /mnt/ext ntfs-3g uid=twoonesecond,gid=wheel,dmask=022,fmask=133 0 0" >> /etc/fstab
+echo -n "$vpass1" > /etc/ext
+echo -e "\next /dev/vda4 /etc/ext tcrypt-veracrypt" >> /etc/crypttab
+
+### Installed the specified microcode
+paru --noconfirm --needed -S "$cpu-ucode"
+
+### Disable COW for virt-manager directory before it is filled
+mkdir -p /var/lib/libvirt/images
+chattr +C /var/lib/libvirt/images
+
+### Run the program installation loop
+installationloop 
+
+### Use lowercase letters for xdg dirs
+sudo -u "$name" mkdir -p /home/$name/.config
+sudo -u "$name" echo -e 'XDG_DESKTOP_DIR="$HOME/desktop"\nXDG_DOWNLOAD_DIR="$HOME/downloads"\nXDG_TEMPLATES_DIR="$HOME/templates"\nXDG_PUBLICSHARE_DIR="$HOME/public"\nXDG_DOCUMENTS_DIR="$HOME/documents"\nXDG_MUSIC_DIR="$HOME/music"\nXDG_PICTURES_DIR="$HOME/pictures"\nXDG_VIDEOS_DIR="$HOME/videos' > /home/$name/.config/user-dirs.dirs
+sudo -u "$name" echo "enabled=False" >> /home/$name/.config/user-dirs.conf
+
+### Setup profiles for snapper
+#### Create profiles for root and home
+snapper -c root create-config /
+snapper -c home create-config /home
+
+#### Disable timeline snapshots in root
+sed -i 's/TIMELINE_CREATE="yes"/TIMELINE_CREATE="no"/' /etc/snapper/configs/root
+
+#### Limit to 20 snapshots for root and home
+sed -i 's/NUMBER_LIMIT="50"/NUMBER_LIMIT="20"/' /etc/snapper/configs/root
+sed -i 's/NUMBER_LIMIT="50"/NUMBER_LIMIT="20"/' /etc/snapper/configs/home
+
+### Remove subvolid from fstab to allow restoring from backup
+sed -i 's/subvolid=.*,//' /etc/fstab
+
+### Set zsh as the default shell for the user
+chsh -s /bin/zsh "$name" >/dev/null 2>&1
+sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
+
+### Set hostname
+hostnamectl set-hostname "$myhostname"
+
+### Set network config
+echo -e "127.0.0.1 localhost\n127.0.1.1 $myhostname"
+
+### Set timezone and clock
+timedatectl set-timezone America/Chicago
+timedatectl set-local-rtc 1
+hwclock --systohc
+systemctl enable systemd-timesyncd
+
+### Remove the beep
+rmmod pcspkr
+echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
+
+# Enable snapper timeline and cleanup
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
+
+# Enable cron
+systemctl enable cronie
+
+# Enable btrfs in grub
+systemctl enable grub-btrfs.path
+
+# Enable sddm
+systemctl enable sddm
+
+# Enable upower
+systemctl enable upower
+
+# Enable libvirt 
+systemctl enable libvirtd
+
+# Enable docker
+systemctl enable docker
+
+# Enable cups (printer)
+systemctl enable cups
+
+# Enable auto cpufreq
+systemctl enable auto-cpufreq
+
+# Enable thermald
+systemctl enable thermald
+
+# Enable sshd
+systemctl enable sshd
+
+# Enable bluetooth
+systemctl enable bluetooth
+
+### Update grub config after install grub-btrfs
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Set npm global install location
+mkdir "/home/$name/.local"
+npm config set prefix "/home/$name/.local"
+
+# Copy over wallpapers
+sudo -u "$name" mkdir /home/$name/pictures/
+git clone "$wallpaperrepo" "/home/$name/pictures/wallpaper"
+
+# Install good fonts
+maininstall "nerd-fonts-complete" "lots of fonts"
+fc-cache -fv
+
+# Allow wheel users to sudo with password and allow several system commands
+# (like `shutdown` to run without password).
+echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/larbs-wheel-can-sudo
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -u -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-larbs-cmds-without-password
+
+echo "Finished running completely!"
+
+## How to deal with git stow and dotfiles? You have no valid ssh cert with github at time of creation...
+## Kinda want lowercase file structure...
